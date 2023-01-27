1a0b55a6b6ce2352ba242f8ff8345b46fc7a6dc7
xX7 <5272979+xX7@users.noreply.github.com>
2023-01-27 20:30:11 +0100

    - adjusted bao makefile - added stress-ng to buildroot conf - updated submodules

--- bao.mk
+++ bao.mk
@@ -11,12 +11,10 @@ $(bao_cfg_repo):
 $(bao_cfg_dest): | $(bao_cfg_repo)
 	cp -L $(bao_cfg) $(bao_cfg_dest)
 
-$(bao_image): $(bao_cfg_dest) $(bao_src) 
+bao $(bao_image): $(bao_cfg_dest) $(bao_src) 
 	$(MAKE) -C $(bao_src)\
 		PLATFORM=$(PLATFORM)\
 		CONFIG_REPO=$(bao_cfg_repo)\
 		CONFIG=$(bao_cfg_file) CONFIG_BUILTIN=y\
 		CPPFLAGS=-DIMG_PATH=$(imgPath)
 	cp $(bao_src)/bin/$(PLATFORM)/builtin-configs/$(bao_cfg_file)/bao.bin $(bao_image)
-
-bao: $(bao_image)
